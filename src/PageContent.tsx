b3a00ee7a09ae221743eb676e11dac6a1749cb6e
Michael Batruch <michael.batruch@edealer.ca>
2023-08-22 21:29:42 -0400

    test push for material ui

--- src/PageContent.tsx
+++ src/PageContent.tsx
@@ -1,11 +1,44 @@
+import styled from 'styled-components/macro';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Podcasts from './Podcasts';
 import Contact from './Contact';
 import PodCastSingle from 'PodcastSingle';
+import { useEffect, useState } from 'react';
+import { Box, Drawer } from '@mui/material';
 
-function PageContent() {
+enum DrawerType {
+  RIGHT_SIDE_MENU = 'right-side-menu',
+}
+
+const SideMenu = styled(Drawer)`
+  z-index: 1090;
+`;
+
+const StyledBox = styled(Box)`
+  z-index: 1090;
+`;
+
+const  PageContent = () => {
+
+  const [sidebarNoticeActive, setSidebarNotice] = useState<boolean>(false);
+
+  const sidebarToggle = () => {
+    setSidebarNotice(!sidebarNoticeActive);
+  };
+
+  useEffect(() => {
+    // setSidebarNotice(!sidebarNoticeActive);
+    console.log(sidebarNoticeActive)
+  }, [
+    // setSidebarNotice,
+    sidebarNoticeActive,
+  ]);
+
+  const anchorRight = 'right';
+  // const drawerBleeding = 56;
+
   return (
     <div>
       <Routes>
@@ -15,6 +48,48 @@ function PageContent() {
         <Route path="contact" element={<Contact />} />
         <Route path="podcasts/:name" element={<PodCastSingle />} />
       </Routes>
+      <SideMenu
+          data-testid={DrawerType.RIGHT_SIDE_MENU}
+          anchor={anchorRight}
+          open={sidebarNoticeActive}
+          hideBackdrop={true}
+          onClick={() => sidebarToggle()}
+          style={{ zIndex: 190, overflow: 'auto' }}
+          sx={{
+            '& .MuiPaper-root': {
+              overflow: 'initial',
+              top: 0,
+              marginTop: 'auto',
+              bottom: 0,
+              marginBottom: 'auto',
+              height: '50%',
+            },
+          }}
+          ModalProps={{
+            keepMounted: true
+          }}
+        >
+          <StyledBox
+              sx={{
+                position: 'absolute',
+                right: '200px',
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+                visibility: 'visible',
+                left: 'auto',
+                width: '100%',
+                top: '50%',
+                '& .MuiPaper-root': {
+                  overflow: 'initial',
+                },
+              }}
+            >
+          <h1
+            onClick={() => sidebarToggle()}
+            >Psst! Over here! 🤫</h1>
+        </StyledBox>
+          <h1>Visit the real Podscripts site here!</h1>
+        </SideMenu>
     </div>
   );
 }
