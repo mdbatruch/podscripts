186b1b8d23e49ef95be0e34788df1c8db4af8c59
Michael Batruch <michael.batruch@edealer.ca>
2023-08-31 21:05:57 -0400

    begin updating podcasts main entity page

--- src/PodcastSingle.tsx
+++ src/PodcastSingle.tsx
@@ -1,12 +1,51 @@
+import Main from 'components/core/sections/page/MainContent';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import { BLUE, WHITE } from 'styles/color';
+import { SPACE_20, SPACE_40 } from 'styles/spacing';
+import BreadCrumbs, { formatPagePath } from 'utils/BreadcrumbUtil';
 
-function PodCastSingle() {
+const PodcastSingleWrapper = styled.div`
+    ${Main};
+    flex-direction: column;
+`;
+
+const PodcastSingleMain = styled.div`
+    background-color: ${WHITE};
+    flex-direction: column;
+    padding: ${SPACE_40};
+    margin-top: ${SPACE_20};
+`;
+
+const PodcastSingleTitle = styled.h1`
+  position: relative;
+  &::before {
+      background: ${BLUE};
+      border-radius: 4px;
+      bottom: -13px;
+      content: '';
+      height: 4px;
+      left: 0;
+      position: absolute;
+      right: 0;
+      display: block;
+      width: 50px;
+  }
+}
+`;
+
+const PodCastSingle = () => {
   const { name } = useParams();
 
+  const newItem = formatPagePath(name);
+
   return (
-    <div>
-      <h1>Podcast {name} </h1>
-    </div>
+    <PodcastSingleWrapper>
+      <BreadCrumbs />
+      <PodcastSingleMain>
+        <PodcastSingleTitle>{newItem}</PodcastSingleTitle>
+      </PodcastSingleMain>
+    </PodcastSingleWrapper>
   );
 }
 export default PodCastSingle;
---
+++ src/utils/BreadcrumbUtil.tsx
@@ -0,0 +1,78 @@
+import { useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom'
+import styled from 'styled-components/macro';
+import { BLUE, WHITE } from 'styles/color';
+import { SPACE_20 } from 'styles/spacing';
+
+
+const BreadCrumbWrapper = styled.div`
+  background-color: ${BLUE};
+  border-radius: 4px;
+  color: ${WHITE};
+  padding: ${SPACE_20};
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const LinkNoDeco = styled(Link)`
+  text-decoration: none;
+  color: ${WHITE};
+  line-height: 1.5;
+  list-style-type: circle;
+  margin-right: ${SPACE_20};
+  position: relative;
+  &:not(:last-child) {
+    &::after {
+        background-color: ${WHITE};
+        height: 4px;
+        width: 4px;
+        content: "";
+        right: -12px;
+        top: calc(50% - 1px);
+        position: absolute;
+        border-radius: 50%;
+    }
+    }
+`;
+
+export const formatPagePath = (item: string | undefined): string => {
+    if (!item) return '';
+
+    const format = item?.split('_')
+    .map(path =>
+        path.charAt(0).toUpperCase() + path.slice(1)
+        )
+    .join(" ");
+
+    return format;
+};
+
+const BreadCrumbs = () => {
+
+    const location = useLocation();
+
+    const pagePath = location.pathname.split('/');
+
+    const items = useMemo(() => pagePath.map((item, idx) => {
+
+        const newItem = formatPagePath(item);
+
+        const breadcrumbPath = `${pagePath.slice(0, idx + 1).join('/')}`;
+        return (
+            item === '' ? (
+                <LinkNoDeco key={'Home'} to={'/'}>Home</LinkNoDeco>
+            ) : (
+                <LinkNoDeco key={item} to={breadcrumbPath}>{newItem}</LinkNoDeco>
+            )
+        );
+    }), [pagePath]);
+
+    return (
+        <BreadCrumbWrapper>
+            {items}
+        </BreadCrumbWrapper>
+    );
+  };
+
+
+export default BreadCrumbs;
